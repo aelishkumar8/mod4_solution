a4d822eae0f08766a058e2c181ab4ad55bd6e459
aelishkumar8 <aelishkumar8@gmail.com>
2022-05-24 23:44:52 +0530

    removing

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ Name[7] = "Adiyta";
 Name[8] = "Sanskar";
 Name[9] = "kumar";
 Name[10] = "Jack";
-console.log(Name);
 
 for(var i = 1; i<11; i++){
     if (Name[i].charAt(0) == "j" || Name[i].charAt(0) == "J") {
